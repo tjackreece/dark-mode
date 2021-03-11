46811663085374c0b68e2dd2b29774cff90c3046
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-03-11 11:57:06 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-
+// import useLocalStorage from "./hooks/useLocalStorage";
+import useDarkMode from "./hooks/useDarkMode";
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
 import "./styles.scss";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+	const [coinData, setCoinData] = useState([]);
+	const [darkMode, setDarkMode] = useDarkMode();
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
-  return (
-    <div className={darkMode ? "dark-mode App" : "App"}>
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-      <Charts coinData={coinData} />
-    </div>
-  );
+	useEffect(() => {
+		axios
+			.get(
+				"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+			)
+			.then((res) => setCoinData(res.data))
+			.catch((err) => console.log(err));
+	}, []);
+	return (
+		<div className={darkMode ? "dark-mode App" : "App"}>
+			<Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+			<Charts coinData={coinData} />
+		</div>
+	);
 };
 
 const rootElement = document.getElementById("root");
